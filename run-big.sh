bf1eea50b1193cb7d19cd3c918719fd16a8a7d44
Mohamed Seada <mohamed.seada.1994@gmail.com>
2018-10-30 17:59:39 +0200

    Add auto File Remove

--- run-big.sh
+++ run-big.sh
@@ -1,3 +1,5 @@
+hdfs dfs -rm -r output/big
+
 $HD_STREAM -input 'input/big.txt' -output 'output/big' -mapper 'node map.js hope' -reducer 'node reduce.js' -file `pwd`/map.js -file `pwd`/reduce.js
 
 hdfs dfs -cat output/big/*
--- run-small.sh
+++ run-small.sh
@@ -1,3 +1,5 @@
+hdfs dfs -rm -r output/small
+
 $HD_STREAM -input 'input/small.txt' -output 'output/small' -mapper 'node map.js hope' -reducer 'node reduce.js' -file `pwd`/map.js -file `pwd`/reduce.js
 
 hdfs dfs -cat output/small/*
